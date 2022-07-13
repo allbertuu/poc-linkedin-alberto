5ca9c88e79a0a29c76c389f5f85476700d7e9d6c
Alberto <albertovinicius3@gmail.com>
2022-07-13 17:00:56 -0300

    chore: environment setup finished

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Button } from './styles';
+
+test('componente Button permite cliques', () => {
+  render(<Button />);
+  const ButtonElement = screen.getByRole('button')
+  expect(ButtonElement).toBeEnabled();
+});
+
+test('renderizar componente Button', () => {
+  render(<Button />);
+  const ButtonElement = screen.getByRole('button')
+  expect(ButtonElement).toBeInTheDocument();
+});
